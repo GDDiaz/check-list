d2e69735d82e72ce9c8ba6c84aa57bfbffcd836f
jdiaz <jdiaz@tiqal.com>
2019-08-22 22:35:15 -0500

    personalizacion d eformularios

--- lib/form/doctrine/CheckListForm.class.php
+++ lib/form/doctrine/CheckListForm.class.php
@@ -12,5 +12,12 @@ class CheckListForm extends BaseCheckListForm
 {
   public function configure()
   {
+    $this->widgetSchema['name'] = new sfWidgetFormInputText(array(), array( 'class' => 'form-control'));
+    $this->widgetSchema['descriptor'] = new sfWidgetFormTextarea(array(), array( 'class' => 'form-control'));
+    $this->widgetSchema['prefix'] = new sfWidgetFormInputText(array(), array( 'class' => 'form-control'));
+    $this->widgetSchema['threshold'] = new sfWidgetFormInputText(array(), array( 'class' => 'form-control'));
+    $this->widgetSchema['status'] = new sfWidgetFormTextarea(array(), array( 'class' => 'form-control'));
+
+    $this->useFields(['name', 'descriptor', 'prefix', 'threshold', 'status']);
   }
 }
