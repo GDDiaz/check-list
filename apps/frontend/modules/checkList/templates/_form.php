d5d4cb1a536695e7ccae44766dad19fe46c2cf06
jdiaz <jdiaz@tiqal.com>
2019-08-23 23:26:10 -0500

    cambio de estilo en los formularios a dos columnas

--- apps/frontend/modules/checkList/templates/_form.php
+++ apps/frontend/modules/checkList/templates/_form.php
@@ -5,20 +5,35 @@
 <?php if (!$form->getObject()->isNew()): ?>
 <input type="hidden" name="sf_method" value="put" />
 <?php endif; ?>
-  <table>
-    <tfoot>
-      <tr>
-        <td colspan="2">
-          &nbsp;<a href="<?php echo url_for('checkList/index') ?>">Back to list</a>
-          <?php if (!$form->getObject()->isNew()): ?>
-            &nbsp;<?php echo link_to('Delete', 'checkList/delete?id='.$form->getObject()->getId(), array('method' => 'delete', 'confirm' => 'Are you sure?')) ?>
-          <?php endif; ?>
-          <input type="submit" value="Save" />
-        </td>
-      </tr>
-    </tfoot>
-    <tbody>
-      <?php echo $form ?>
-    </tbody>
-  </table>
+  <div class="row">
+    <div class="col m6">
+    <?php echo $form['name']->renderLabel() ?>
+      <?php echo $form['name'] ?>
+      <?php echo $form['name']->renderError() ?>
+      <?php echo $form['descriptor']->renderLabel() ?>
+      <?php echo $form['descriptor'] ?>
+      <?php echo $form['descriptor']->renderError() ?>
+      <?php echo $form['prefix']->renderLabel() ?>
+      <?php echo $form['prefix'] ?>
+      <?php echo $form['prefix']->renderError() ?>
+    </div>
+    <div class="col m6">
+      <?php echo $form['threshold']->renderLabel() ?>
+      <?php echo $form['threshold'] ?>
+      <?php echo $form['threshold']->renderError() ?>
+      <?php echo $form['status']->renderLabel() ?>
+      <?php echo $form['status'] ?>
+      <?php echo $form['status']->renderError() ?>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col m12">
+      &nbsp;<a href="<?php echo url_for('checkList/index') ?>">Back to list</a>
+      <?php if (!$form->getObject()->isNew()): ?>
+         &nbsp;<?php echo link_to('Delete', 'checkList/delete?id='.$form->getObject()->getId(), array('method' => 'delete', 'confirm' => 'Are you sure?')) ?>
+      <?php endif; ?>
+      <input type="submit" value="Save" />
+
+    </div>
+  </div>
 </form>
